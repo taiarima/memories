b964b4dd0e392956faec95fcc9747a5594ff7f53
taiarima <tai.arima@colorado.edu>
2023-09-13 16:16:50 +0300

    Added features, contact form.

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,10 +1,51 @@
 import NavBar from "../components/NavBar";
+import Button from "../components/Button";
 
 function Contact() {
   return (
     <div>
       <NavBar />
-      <h1>Contact Page</h1>
+      <div name="contact" className="w-full p-4 pt-40 text-white">
+        <div className="mx-auto flex h-full max-w-screen-lg flex-col justify-center p-4">
+          <div className="pb-8">
+            <p className="inline border-b-4 border-gray-500 text-4xl font-bold">
+              Contact
+            </p>
+            <p className="py-4">
+              Fill out the form below to ask questions or receive support
+            </p>
+          </div>
+          <div className="flex items-center justify-center ">
+            <form
+              className="flex w-full flex-col md:w-1/2"
+              action=""
+              method="POST"
+            >
+              <input
+                type="text"
+                name="name"
+                placeholder="Your name"
+                className="my-2 rounded-md border-2 bg-customTeal p-2 font-bold text-black placeholder:text-slate-600 focus:outline-none"
+              />
+              <input
+                type="text"
+                name="email"
+                placeholder="Your email address"
+                className="my-2 rounded-md border-2 bg-customTeal p-2 font-bold text-black placeholder:text-slate-600 focus:outline-none"
+              />
+              <textarea
+                name="message"
+                placeholder="Enter your message"
+                rows="10"
+                className="my-2 rounded-md border-2 bg-customTeal p-2 font-bold text-black placeholder:text-slate-600 focus:outline-none"
+              ></textarea>
+              <div className="text-center">
+                <Button>Submit</Button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,10 +4,27 @@ function Home() {
   return (
     <>
       <NavBar />
-      <div className="m-20 flex h-48 flex-col justify-center rounded-lg font-title">
-        <h1 className="text-center text-6xl text-customCream">
+      <div className="flex flex-col justify-center rounded-lg font-title">
+        <h1 className="mt-48 text-center text-6xl text-customCream">
           Keep your journal, one memory at a time.
         </h1>
+        <div className="m-16 text-center font-title text-white">
+          <h2 className="text-4xl">Features</h2>
+          <ul className="my-4 space-y-4 text-2xl">
+            <li>
+              &#x2022; Tons of daily prompts so you never get stumped what to
+              write about
+            </li>
+            <li>
+              &#x2022; Keep your journal private or share it with friends and
+              family
+            </li>
+            <li>
+              &#x2022; All the little memories you want to keep in one
+              convenient location
+            </li>
+          </ul>
+        </div>
       </div>
     </>
   );
